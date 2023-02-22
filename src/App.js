0dd63f0f38778a907c69464dc692595767d811ad
Elena Kuzmitskaya <e.kuzmitskaya@gmail.com>
2023-02-22 19:24:24 +0300

    basic style

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ function App() {
       <div className="calculator-body basic-theme">
         <div className="input-block">
           <input className="basic-theme" id='input-field'></input>
-
         </div>
+
         <div className="buttons-section basic-theme">
-            <button className="btn" id='number-0'>0</button>
-            <button className="btn" id='number-1'>1</button>
-            <button className="btn" id='number-2'>2</button>
-            <button className="btn" id='number-3'>3</button>
-            <button className="btn" id='number-4'>4</button>
-            <button className="btn" id='number-5'>5</button>
-            <button className="btn" id='number-6'>6</button>
-            <button className="btn" id='number-7'>7</button>
-            <button className="btn" id='number-8'>8</button>
-            <button className="btn" id='number-9'>9</button>
-            <button className="btn" id='number-comma'>,</button>
+          <button className="btn" id='number-0'>0</button>
+          <button className="btn" id='number-1'>1</button>
+          <button className="btn" id='number-2'>2</button>
+          <button className="btn" id='number-3'>3</button>
+          <button className="btn" id='number-4'>4</button>
+          <button className="btn" id='number-5'>5</button>
+          <button className="btn" id='number-6'>6</button>
+          <button className="btn" id='number-7'>7</button>
+          <button className="btn" id='number-8'>8</button>
+          <button className="btn" id='number-9'>9</button>
+          <button className="btn" id='number-comma'>,</button>
+
           <button className="btn" id='btn-plus'>+</button>
           <button className="btn" id='btn-minus'>-</button>
           <button className="btn" id='btn-times'>X</button>
